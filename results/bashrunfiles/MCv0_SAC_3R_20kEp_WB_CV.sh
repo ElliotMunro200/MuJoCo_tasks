342fc661026d9ad2f4662170ab7f6e0b47d29606
ElliotMunro200 <74408400+ElliotMunro200@users.noreply.github.com>
2022-06-14 10:59:11 +1200

    minor changes

--- results/bashrunfiles/MCv0_SAC_3R_20kEp_WB_CV.sh
+++ results/bashrunfiles/MCv0_SAC_3R_20kEp_WB_CV.sh
@@ -1,12 +1,12 @@
 
 
 python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=SAC --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=1
+                                    --wandb --capture_video --seed=1
 
 python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=SAC --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=2
+                                    --wandb --capture_video --seed=2
 
 python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=SAC --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=3
+                                    --wandb --capture_video --seed=3
 
 
