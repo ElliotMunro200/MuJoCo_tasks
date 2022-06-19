beb8772a73a9cbb8aec4be18eda6aaaed1abd216
ElliotMunro200 <74408400+ElliotMunro200@users.noreply.github.com>
2022-06-20 09:47:15 +1200

    DIAYN.sh

--- results/bashrunfiles/DIAYN_3R_20kEp_WB_CV_GPU.sh
+++ results/bashrunfiles/DIAYN_3R_20kEp_WB_CV_GPU.sh
@@ -1,52 +1,61 @@
 
-#DDPG
-#Hopper
-python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=DDPG --num_episodes_per_run=20000 \
+#DIAYN
+#MountainCarContinuous
+python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=DIAYN --num_episodes_per_run=2000 \
                                     --wandb --capture_video --GPU --seed=1
 
-python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=DDPG --num_episodes_per_run=20000 \
+python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=DIAYN --num_episodes_per_run=2000 \
                                     --wandb --capture_video --GPU --seed=2
 
-python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=DDPG --num_episodes_per_run=20000 \
+python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=DIAYN --num_episodes_per_run=2000 \
                                     --wandb --capture_video --GPU --seed=3
 
-#Walker2d
-python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=DDPG --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=1
-
-python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=DDPG --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=2
-
-python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=DDPG --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=3
-
-#TD3
 #Reacher
-python results/MuJoCo_experiment.py --env_id=Reacher-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
+python results/MuJoCo_experiment.py --env_id=Reacher-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
                                     --wandb --capture_video --GPU --seed=1
 
-python results/MuJoCo_experiment.py --env_id=Reacher-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=2 
-
-python results/MuJoCo_experiment.py --env_id=Reacher-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=3
-
-#Hopper
-python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
-                                    --wandb --capture_video --GPU --seed=1
-
-python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
+python results/MuJoCo_experiment.py --env_id=Reacher-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
                                     --wandb --capture_video --GPU --seed=2
 
-python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
+python results/MuJoCo_experiment.py --env_id=Reacher-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
                                     --wandb --capture_video --GPU --seed=3
 
 #Walker2d
-python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
+python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
                                     --wandb --capture_video --GPU --seed=1
 
-python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
+python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
                                     --wandb --capture_video --GPU --seed=2
 
-python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=TD3 --num_episodes_per_run=20000 \
+python results/MuJoCo_experiment.py --env_id=Walker2d-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=3
+
+#Hopper
+python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=1
+
+python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=2
+
+python results/MuJoCo_experiment.py --env_id=Hopper-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=3
+
+#HalfCheetah
+python results/MuJoCo_experiment.py --env_id=HalfCheetah-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=1
+
+python results/MuJoCo_experiment.py --env_id=HalfCheetah-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=2
+
+python results/MuJoCo_experiment.py --env_id=HalfCheetah-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=3
+
+#Ant
+python results/MuJoCo_experiment.py --env_id=Ant-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=1
+
+python results/MuJoCo_experiment.py --env_id=Ant-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
+                                    --wandb --capture_video --GPU --seed=2
+
+python results/MuJoCo_experiment.py --env_id=Ant-v4 --agent_name=DIAYN --num_episodes_per_run=20000 \
                                     --wandb --capture_video --GPU --seed=3
