d061e17457c64384b2e6d657d7b11a3c493ae45a
ElliotMunro200 <74408400+ElliotMunro200@users.noreply.github.com>
2022-06-28 15:59:55 +1200

    adding TD3.sh

---
+++ results/bashrunfiles/TD3.sh
@@ -0,0 +1,9 @@
+#MCC
+python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=TD3 --num_episodes_per_run=2000 \
+                                    --wandb --capture_video --seed=1
+
+python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=TD3 --num_episodes_per_run=2000 \
+                                    --wandb --capture_video --seed=2
+
+python results/MuJoCo_experiment.py --env_id=MountainCarContinuous-v0 --agent_name=TD3 --num_episodes_per_run=2000 \
+                                    --wandb --capture_video --seed=3
